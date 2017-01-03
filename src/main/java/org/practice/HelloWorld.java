bb4da882f727899ffccbb3507fa5b25c479ecf07
jasjyotsinghjaswal <jasjyot_singh_jaswal@yahoo.com>
2017-01-04 00:19:06 +0530

    Update HelloWorld.java

--- src/main/java/org/practice/HelloWorld.java
+++ src/main/java/org/practice/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
 	public static void main (String args [])
 	
 	{
-		System.out.println("Hello Worl Maven Edit 3 ");
+		System.out.println("Hello Worl XMaven Edit 3 ");
 		
 	}
 	
